642f622bb360a33c68e323d7a05927af073ec6b9
Joseph Fischetti <fischettijw@gmail.com>
2020-12-18 05:35:06 -0500

    2 planets

--- sketch.js
+++ sketch.js
@@ -1,10 +1,12 @@
 let mover1;
+let mover2;
 let attractor;
 
 
 function setup() {
     createCanvas(500, 500);
     mover1 = new Mover(175, 125, 5); // mover1 = new Mover(50, 350, 5);
+    mover2 = new Mover(175, 375, 5); // mover1 = new Mover(50, 350, 5);
     attractor = new Attractor(width / 2, height / 2, 20);
     background(220);
 }
@@ -14,7 +16,10 @@ function draw() {
     background(220, 5);
     mover1.update();
     mover1.show();
+    mover2.update();
+    mover2.show();
 
     attractor.attract(mover1);
+    attractor.attract(mover2);
     attractor.show();
 }
